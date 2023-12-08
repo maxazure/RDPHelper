14e2dbc4e392c20e13fda0bb0e0daa08ba551169
Liu Jie <maxazure@gmail.com>
2023-12-09 12:33:57 +1300

    Initial commit

---
+++ RDPHelper/MainForm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace RDPHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            comboBoxResolution = new ComboBox();
+            comboBoxColorDepth = new ComboBox();
+            label1 = new Label();
+            label2 = new Label();
+            checkBoxFullScreen = new CheckBox();
+            checkBoxMultiMonitor = new CheckBox();
+            buttonModify = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // comboBoxResolution
+            // 
+            comboBoxResolution.FormattingEnabled = true;
+            comboBoxResolution.Location = new Point(120, 52);
+            comboBoxResolution.Name = "comboBoxResolution";
+            comboBoxResolution.Size = new Size(176, 25);
+            comboBoxResolution.TabIndex = 0;
+            // 
+            // comboBoxColorDepth
+            // 
+            comboBoxColorDepth.FormattingEnabled = true;
+            comboBoxColorDepth.Location = new Point(120, 96);
+            comboBoxColorDepth.Name = "comboBoxColorDepth";
+            comboBoxColorDepth.Size = new Size(176, 25);
+            comboBoxColorDepth.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 17);
+            label1.TabIndex = 2;
+            label1.Text = "分辨率";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 17);
+            label2.TabIndex = 3;
+            label2.Text = "颜色深度";
+            // 
+            // checkBoxFullScreen
+            // 
+            checkBoxFullScreen.AutoSize = true;
+            checkBoxFullScreen.Location = new Point(120, 143);
+            checkBoxFullScreen.Name = "checkBoxFullScreen";
+            checkBoxFullScreen.Size = new Size(51, 21);
+            checkBoxFullScreen.TabIndex = 4;
+            checkBoxFullScreen.Text = "全屏";
+            checkBoxFullScreen.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMultiMonitor
+            // 
+            checkBoxMultiMonitor.AutoSize = true;
+            checkBoxMultiMonitor.Checked = true;
+            checkBoxMultiMonitor.CheckState = CheckState.Checked;
+            checkBoxMultiMonitor.Location = new Point(121, 181);
+            checkBoxMultiMonitor.Name = "checkBoxMultiMonitor";
+            checkBoxMultiMonitor.Size = new Size(111, 21);
+            checkBoxMultiMonitor.TabIndex = 5;
+            checkBoxMultiMonitor.Text = "使用多个显示器";
+            checkBoxMultiMonitor.UseVisualStyleBackColor = true;
+            // 
+            // buttonModify
+            // 
+            buttonModify.Location = new Point(120, 275);
+            buttonModify.Name = "buttonModify";
+            buttonModify.Size = new Size(176, 43);
+            buttonModify.TabIndex = 6;
+            buttonModify.Text = "连接";
+            buttonModify.UseVisualStyleBackColor = true;
+            buttonModify.Click += buttonModify_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(137, 216);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 17);
+            label3.TabIndex = 7;
+            label3.Text = "自动重定向本地麦克风";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 350);
+            Controls.Add(label3);
+            Controls.Add(buttonModify);
+            Controls.Add(checkBoxMultiMonitor);
+            Controls.Add(checkBoxFullScreen);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(comboBoxColorDepth);
+            Controls.Add(comboBoxResolution);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "远程连接设置器";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBoxResolution;
+        private ComboBox comboBoxColorDepth;
+        private Label label1;
+        private Label label2;
+        private CheckBox checkBoxFullScreen;
+        private CheckBox checkBoxMultiMonitor;
+        private Button buttonModify;
+        private Label label3;
+    }
+}
---
+++ RDPHelper/MainForm.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace RDPHelper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // 初始化下拉框等控件
+            comboBoxResolution.Items.Add("1920x1080");
+            comboBoxResolution.Items.Add("2560x1080");
+            comboBoxResolution.Items.Add("3440x1440");
+            comboBoxResolution.Items.Add("3840x1600");
+            comboBoxResolution.Items.Add("1280x720");
+            // 添加其他所需的分辨率选项
+            comboBoxResolution.SelectedIndex = 0;
+            comboBoxResolution.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // 初始化颜色深度下拉框
+            comboBoxColorDepth.Items.Add("16");
+            comboBoxColorDepth.Items.Add("24");
+            comboBoxColorDepth.Items.Add("32");
+            // ...添加其他颜色深度选项
+            comboBoxColorDepth.SelectedIndex = 0;
+            comboBoxColorDepth.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            checkBoxFullScreen.Checked = false;
+            checkBoxMultiMonitor.Checked = false;
+        }
+
+        private void buttonModify_Click(object sender, EventArgs e)
+        {
+            string resolution = comboBoxResolution.SelectedItem.ToString();
+            string colorDepth = comboBoxColorDepth.SelectedItem.ToString();
+            bool fullScreen = checkBoxFullScreen.Checked;
+            bool multiMonitor = checkBoxMultiMonitor.Checked;
+
+            // 获取下载文件夹路径
+            string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+
+            // 找到最新的 .rdp 文件
+            var rdpFiles = new DirectoryInfo(downloadsPath).GetFiles("*.rdp").OrderByDescending(f => f.LastWriteTime).ToList();
+            var latestRdpFile = rdpFiles.FirstOrDefault();
+
+            if (latestRdpFile != null)
+            {
+                string filePath = latestRdpFile.FullName;
+                string[] resSplit = resolution.Split('x');
+                string desktopWidth = resSplit[0];
+                string desktopHeight = resSplit[1];
+
+                // 读取文件内容
+                StringBuilder newContent = new StringBuilder();
+                string[] content = File.ReadAllLines(filePath);
+
+                // 修改内容
+                foreach (string line in content)
+                {
+         
+                    if (line.StartsWith("use multimon:i:"))
+                        newContent.AppendLine($"use multimon:i:" + (multiMonitor ? "1" : "0"));
+                    else
+                        newContent.AppendLine(line);
+                }
+
+                if (!fullScreen)
+                {
+                    newContent.AppendLine($"desktopwidth:i:{desktopWidth}");
+                    newContent.AppendLine($"desktopheight:i:{desktopHeight}");
+                    newContent.AppendLine("disable full window drag:i:1");
+
+                }
+               
+                newContent.AppendLine($"screen mode id:i:" + (fullScreen ? "2" : "1"));
+                newContent.AppendLine($"session bpp:i:{colorDepth}");
+                newContent.AppendLine("audiocapturemode:i:1");
+                newContent.AppendLine("redirectvideo:i:1");
+
+
+                // 将修改后的内容写回文件
+                File.WriteAllText(filePath, newContent.ToString());
+
+
+                // 删除除最新文件之外的所有 .rdp 文件
+                foreach (var file in rdpFiles)
+                {
+                    if (file.FullName != filePath)
+                    {
+                        try
+                        {
+                            File.Delete(file.FullName);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Failed to delete file {file.Name}: {ex.Message}");
+                        }
+                    }
+                }
+
+                // 直接启动远程桌面会话
+                try
+                {
+                    Process.Start("mstsc.exe", $"\"{filePath}\"");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to start remote desktop session: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No RDP file found in Downloads.");
+            }
+        }
+    }
+}
